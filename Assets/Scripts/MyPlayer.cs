f7e382631ee99ce5d59ef631f2e03dd932dfd4d8
Alius <alexa.glinsoo@gmail.com>
2020-10-08 22:32:59 -0300

    first commit

---
+++ Assets/Scripts/MyPlayer.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MyPlayer : MonoBehaviour
+{
+    public Joystick joystick;   
+    public float speed;
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        Movement();
+    }
+
+    void Movement()
+    {
+        var rigdbody = GetComponent<Rigidbody2D>();
+        rigdbody.velocity = new Vector3(joystick.Horizontal * speed, rigdbody.velocity.y, joystick.Vertical * speed);
+    }
+
+    public void Shot()
+    {
+        print("tiro");
+    }
+}
---
+++ Assets/Scripts/SceneTurnCell.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SceneTurnCell : MonoBehaviour
+{
+    public string name;
+    public int time;
+    void Start()
+    {
+        StartCoroutine(NewScene(name, time));
+    }
+
+    void Update()
+    {
+    }
+
+    IEnumerator NewScene(string name, int time)
+    {
+        yield return new WaitForSeconds(time);
+        Application.LoadLevel(name);
+    }
+}
